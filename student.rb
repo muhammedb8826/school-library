fdeca96f48f47c4e8c27cea3e8efedc21f7afedb
Muhammed Berisso <muhammedb8826@gmail.com>
2023-05-27 16:05:34 +0300

    Fixed linter errors

--- student.rb
+++ student.rb
@@ -1,16 +1,17 @@
 require_relative('./person')
 
 class Student < Person
-  attr_accessor :classroom
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @classroom = classroom
   end
 
-def classroom=(classroom)
-  @classroom = classroom
-  classroom.students.push(self) unless classroom.students.include?(self)
-end
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
   def play_hooky
     '¯\(ツ)/¯'
   end
